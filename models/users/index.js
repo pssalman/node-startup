98ed1b0604df730b4f4e9d99002a255488b88c2d
Tony Salman <anton.salman@gmail.com>
2018-10-17 21:49:54 +0300

    fixed bycrypt library dependancy

--- models/users/index.js
+++ models/users/index.js
@@ -1,19 +1,26 @@
 const mongoose = require('mongoose');
+const timestamps = require('mongoose-timestamp');
+const uniqueValidator = require('mongoose-unique-validator');
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
     _id: mongoose.Types.ObjectId,
     email: { 
-        type: String, 
+        type: String,
+        trim: true,
+		lowercase: true,
         required: true, 
         unique: true, 
         match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/ 
     },
-    username: { type: String, required: true, unique: true, },
+    username: { type: String, trim: true, required: true, unique: true, },
     name: {
-        first_name: { type: String, },
-        last_name: { type: String, },
+        first_name: { type: String, trim: true },
+        last_name: { type: String, trim: true },
     },
     password: { type: String, required: true, },
-});
+}, { collection: 'users' });
 
-module.exports = mongoose.model('User', userSchema);
+UserSchema.plugin(timestamps);
+UserSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('User', UserSchema);
